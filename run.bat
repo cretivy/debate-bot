1a679f89f774684ba71765694f704293adbbcc30
cretivy <abdullayevakobir.123@gmail.com>
2026-03-04 22:14:37 +0500

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Talk Clash Botini ishga tushirmoqdaman...
-if not exist .venv (
-    echo [Xatolik] .venv papkasi topilmadi! Birinchi loyihani sozlab oling.
-    pause
-    exit /b
-)
-.\.venv\Scripts\python.exe main.py
-pause
